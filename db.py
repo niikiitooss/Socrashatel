fe20d2567fe69c53880fe9bf58129e6d29c7c86c
niikiitooss <nikiitooss@mail.ru>
2023-11-14 20:52:27 +0500

    git commit -m "Socrashatel"

--- db.py
+++ db.py
@@ -1,7 +1,9 @@
 import sqlite3
 
 connect = sqlite3.connect('database.db', check_same_thread=False)
+
 cursor = connect.cursor()
+
 cursor.execute('''CREATE TABLE IF NOT EXISTS "users"
 ("id" INTEGER NOT NULL,
 "login" TEXT NOT NULL,
@@ -14,8 +16,9 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS "links"
 ("id" INTEGER NOT NULL,
 "long" TEXT NOT NULL,
 "short" TEXT NOT NULL,
+"count" INT NOT NULL,
 "access_id" INTEGER NOT NULL,
-"owner_id" INTEGER NOT NULL,
+"user_id" INTEGER NOT NULL,
 primary key ("id" AUTOINCREMENT)
 );''')
 connect.commit()
@@ -28,25 +31,12 @@ primary key ("id" AUTOINCREMENT)
 );''')
 connect.commit()
 
-#регистрация
-def registration(login,password):
+def registr(login,password):
     cursor.execute('''INSERT INTO
         users (login,password)
         VALUES (?,?) 
-        ''', (login,password,))
+        ''', (login,password))
     connect.commit()
-
-def searchUser(login):
-    return cursor.execute('''SELECT password 
-        FROM users
-        WHERE login = ? 
-        ''', (login,)).fetchone()
-
-def searchUserId(login):
-    return cursor.execute('''SELECT id 
-        FROM users
-        WHERE login = ? 
-        ''', (login,)).fetchone()
 
 def auth(login,password):
     cursor.execute('''SELECT * 
@@ -55,3 +45,84 @@ def auth(login,password):
         AND password = ?
         ''', (login, password,)).fetchone()
     return "Вы вошли"
+
+def getUser(login):
+    return cursor.execute('''SELECT password 
+        FROM users
+        WHERE login = ? 
+        ''', (login,)).fetchall()
+
+def getUserId(login):
+    return cursor.execute('''SELECT id 
+        FROM users
+        WHERE login = ? 
+        ''', (login,)).fetchone()
+
+def addLink(long,short,access_id,user_id,count = 0):
+    cursor.execute('''INSERT INTO
+        links (long,short,count,access_id,user_id)
+        VALUES (?,?,?,?,?)
+        ''', (long,short,count,access_id,user_id))
+    connect.commit()
+
+def getAccesses():
+    return cursor.execute('''SELECT * FROM accesses
+    ''',()).fetchall()
+
+def addAccesses(level_eng,level_ru):
+    cursor.execute('''INSERT INTO
+        accesses (level_eng,level_ru)
+        VALUES (?,?)
+        ''', (level_eng,level_ru))
+    connect.commit()
+    return "Добавление категории прошло успешно"
+
+def getUserLinks(user_id):
+    return cursor.execute('''SELECT links.long, links.short, links.count, accesses.level_ru, accesses.id
+    FROM links
+    INNER JOIN accesses ON accesses.id = links.access_id
+    WHERE links.user_id = ?
+    ''', (user_id,)).fetchall()
+
+def getPseudonym(pseudonym):
+    return cursor.execute('''SELECT short 
+    FROM links
+    WHERE short = ?''',(pseudonym,)).fetchall()
+
+def getLongUser(long_link, user_id):
+    return cursor.execute('''SELECT long 
+    FROM links
+    WHERE long = ? AND user_id = ?''',(long_link,user_id)).fetchall()
+
+def getInfoLink(user_id, long_link):
+    return cursor.execute('''SELECT long, short, count, access_id
+    FROM links 
+    WHERE user_id = ? AND long = ?''',(user_id,long_link)).fetchall()
+
+def updateLink(long,short,access_id,user_id):
+    cursor.execute('''UPDATE links
+    SET short = ?, access_id = ?
+    WHERE user_id = ? AND long = ?''',(short,access_id,user_id,long))
+    connect.commit()
+
+def deleteLink(long_link, user_id):
+    cursor.execute('''DELETE
+    FROM links
+    WHERE long = ? AND user_id = ?''',(long_link,user_id))
+    connect.commit()
+    return "Удалил"
+
+def getLinkInfo(short):
+    return cursor.execute('''SELECT long, count, access_id, user_id
+    FROM links
+    WHERE short = ?''',(short,)).fetchall()
+
+def updateCount(long,count):
+    cursor.execute('''UPDATE links
+    SET count = ?
+    WHERE long = ?''',(count,long))
+    connect.commit()
+
+def accessesInfo():
+    return cursor.execute('''SELECT level_ru
+    FROM accesses''').fetchall()
--- main.py
+++ main.py
@@ -1,15 +1,44 @@
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, redirect, session
 from db import *
-from flask_jwt_extended import create_access_token, JWTManager, get_jwt_identity, jwt_required
-import uuid, hashlib, random, os
+import hashlib, random, os
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__, template_folder='templates', static_folder='templates/static')
+app.secret_key = os.urandom(30).hex()
+app.config['JWT_SECRET_KEY'] = 'super-secret'
+
+if len(getAccesses()) == 0:
+    i = 0
+    accesses = [["public","публичный"],["general","общий"],["private","приватный"]]
+    while i < len(accesses):
+        addAccesses(accesses[i][0],accesses[i][1])
+        i = i + 1
 
 @app.route('/', methods=['post', 'get'])
 def index():
-
-    return render_template("index.html")
+    long_link = request.form.get('long_link')
+    accesses = request.form.get('accesses')
+    pseudonym = request.form.get('pseudonym')
+    massage = ''
+    if long_link != None:
+        if 'user' in session:
+            userLongLink = getLongUser(long_link, session['user'])
+            print(getLongUser(long_link, session['user']))
+            if len(userLongLink) == 0:
+                if pseudonym:
+                    checkPseudonym = getPseudonym(pseudonym)
+                    if len(checkPseudonym) == 0:
+                        addLink(long_link, pseudonym, accesses, session['user'])
+                    else:
+                        massage = 'Данный псевдоним занят'
+                else:
+                    user_short_link = hashlib.md5(long_link.encode()).hexdigest()[:random.randint(8, 12)]
+                    addLink(long_link,user_short_link,accesses,session['user'])
+            else:
+                massage = 'Вы уже сокращали эту ссылку'
+        else:
+            massage = 'Войдите чтобы сокращать ссылки'
+    return render_template("index.html", massage=massage)
 
 @app.route('/auth', methods=['post', 'get'])
 def auth():
@@ -17,20 +46,19 @@ def auth():
     if request.method == 'POST':
         login = request.form.get('login')
         password = request.form.get('password')
-        user = searchUser(login)
-        print(user)
+        user = getUser(login)
         if login != "" and password != "":
-            if user != None:
-                if check_password_hash(user[0], password) == True:
-                    massage = 'Успешный вход'
-                    auth_user = searchUserId(login)[0]
+            if len(user) > 0:
+                if check_password_hash(user[0][0], password) == True:
+                    auth_user = getUserId(login)[0]
+                    session['user'] = auth_user
                     return redirect('/profile')
                 else:
-                    massage = 'Неверный пароль'
+                    massage = 'Пароль неверный'
             else:
-                massage = 'Такой пользователь не зарегистрирован '
+                massage = 'Такой пользователя нет'
         else:
-            massage = 'Неверный логин / пароль'
+            massage = 'Неверный логин или пароль'
 
     return render_template("auth.html", massage=massage)
 
@@ -41,24 +69,61 @@ def reg():
         login = request.form.get('login')
         password = request.form.get('password')
         confirmPassword = request.form.get('confirm_password')
-        user = searchUser(login)
+        user = getUser(login)
+        print(user)
         if login != "" and password != "":
-            if user == None:
+            if len(user) == 0:
                 if confirmPassword == password:
                     hash_password = generate_password_hash(password)
-                    registration(login, hash_password)
+                    registr(login, hash_password)
                 else:
                     massage = 'Пароли не совпадают'
             else:
                 massage = 'Такой пользователь уже зарегестрирован'
         else:
-            massage = 'Неверный логин / пароль'
+            massage = 'Неверный логин или пароль'
 
     return render_template("reg.html", massage=massage)
 
 @app.route('/profile', methods=['post', 'get'])
 def profile():
-    return render_template("/profile.html")
+    long_name = request.form.get('long_name')
+    edit_long_name = request.form.get('edit_long_name')
+    userlinks = getUserLinks(session['user'])
+    hosthref = request.host_url
+
+    if long_name:
+        deleteLink(long_name,session['user'])
+        return redirect('/profile')
+
+    if edit_long_name:
+        long = request.form.get('long')
+        short = request.form.get('short')
+        pseudonym = request.form.get('pseudonym')
+        access = request.form.get('access')
+        print("ffdfd")
+        if long != None and short != None:
+            if pseudonym == "on":
+                user_short_link = hashlib.md5(long.encode()).hexdigest()[:random.randint(8, 12)]
+                updateLink(long, user_short_link, access, session['user'])
+                print("1")
+                return redirect('/profile')
+            elif pseudonym == None and userlinks[0][2] != access:
+                updateLink(long, short, access, session['user'])
+                print("2")
+                return redirect('/profile')
+            else:
+                if len(getPseudonym(short)) == 0:
+                    updateLink(long, short, access, session['user'])
+                    return redirect('/profile')
+                else:
+                    massage = 'Данный псевдоним занят'
+    return render_template("/profile.html", userlinks=userlinks, hosthref=hosthref)
+
+@app.route('/logout', methods=['post', 'get'])
+def logout():
+    session.clear()
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run()
